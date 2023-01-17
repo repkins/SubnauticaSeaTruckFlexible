a72c0dd3dbfaf54fd0c64e3c8eb21483c2f6b026
Antons Repins <dumperix1993@gmail.com>
2023-01-17 21:35:22 +0200

    fixed cam

---
+++ FixedCamera/FixedCameraController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SubnauticaSeaTruckFlexible.FixedCamera
+{
+    internal class FixedCameraController : MonoBehaviour
+    {
+        private bool isActive = false;
+        private bool toggleNextFrame = false;
+
+        private Transform camTransform;
+        private Transform origCamParentTransform;
+        private Vector3 origCamLocalPosition;
+        private Quaternion origCamLocalOrientation;
+
+        private void Awake()
+        {
+            DevConsole.RegisterConsoleCommand(this, "fixedcam", false, false);
+        }
+
+        private void OnConsoleCommand_fixedcam()
+        {
+            this.toggleNextFrame = true;
+        }
+
+        private void LateUpdate()
+        {
+            if (this.toggleNextFrame)
+            {
+                FixedcamToggle();
+                this.toggleNextFrame = false;
+            }
+        }
+
+        private void FixedcamToggle()
+        {
+            this.isActive = !this.isActive;
+
+            if (this.isActive)
+            {
+                this.camTransform = SNCameraRoot.main.transform;
+                this.origCamParentTransform = this.camTransform.parent;
+
+                this.origCamLocalPosition = this.camTransform.localPosition;
+                this.origCamLocalOrientation = this.camTransform.localRotation;
+
+                this.camTransform.SetParent(null, true);
+            }
+            else
+            {
+                this.camTransform.SetParent(this.origCamParentTransform, true);
+                this.camTransform.localPosition = this.origCamLocalPosition; 
+                this.camTransform.localRotation = this.origCamLocalOrientation;
+            }
+        }
+    }
+}
---
+++ FixedCamera/MainGameControllerPatches.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubnauticaSeaTruckFlexible.FixedCamera
+{
+    [HarmonyPatch(typeof(MainGameController))]
+    internal class MainGameControllerPatches
+    {
+        [HarmonyPatch(nameof(MainGameController.StartGame))]
+        [HarmonyPostfix()]
+        static IEnumerator AddFixedCameraControllerAfterGameLoaded(IEnumerator enumerator)
+        {
+            yield return enumerator;
+
+            var freecamController = UnityEngine.Object.FindObjectOfType<FreecamController>();
+            if (freecamController)
+            {
+                var go = freecamController.gameObject;
+
+                if (!go.TryGetComponent<FixedCameraController>(out var fixedCamera))
+                {
+                    go.AddComponent<FixedCameraController>();
+
+                    Logger.Info($"FixedCameraController added to {go}");
+                }
+                else
+                {
+                    Logger.Info($"Already have FixedCameraController added to {go}");
+                }
+            }
+            else
+            {
+                Logger.Warning($"No FreecamController found.");
+            }
+        }
+    }
+}
